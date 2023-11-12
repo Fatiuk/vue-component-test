90f2eb48a6377cedd2a998a829db4abd68039e04
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-11-12 23:35:23 +0200

    feat: added header menu

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-menu
+    :default-active="activeIndex"
+    class="el-menu-demo"
+    mode="horizontal"
+    :ellipsis="false"
+    :router="true"
+  >
+    <el-menu-item>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="28" height="28" />
+      <h1>Project</h1>
+    </el-menu-item>
+    <div class="flex-grow"></div>
+    <el-menu-item index="/"> Home </el-menu-item>
+    <el-menu-item index="/contacts"> Contacts</el-menu-item>
+  </el-menu>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+onMounted(() => {
+  activeIndex.value = window.location.pathname
+})
+
+const activeIndex = ref('1')
+</script>
+
+<style>
+.flex-grow {
+  flex-grow: 1;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import ElementPlus from 'element-plus'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router).use(ElementPlus)
 
 app.mount('#app')
