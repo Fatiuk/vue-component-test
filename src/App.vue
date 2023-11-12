004ebc3421082ef327bb8b29db528a28d78b2bf5
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-11-12 18:53:21 +0200

    feat: added contacts page

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/contacts">Contacts</RouterLink>
       </nav>
     </div>
   </header>
